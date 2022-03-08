085786e544c7bb646ff52e79e9a3142712420109
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-03-08 13:50:07 +0100

    Added char and boolean primitive data types

---
+++ src/main/java/FirstSteps/CharAndBooleanPrimitive/Main.java
@@ -0,0 +1,15 @@
+package FirstSteps.CharAndBooleanPrimitive;
+
+public class Main {
+    public static void main(String[] args){
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwenyOne = true;
+    }
+}
